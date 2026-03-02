5a5599ee88e1ba159cb99cc2aca36daf7bfc7d04
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-03-02 11:06:38 +0900

    [Bronze V] Title: A+B - 4, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,14 @@
+import sys
+
+while True:
+    try:
+        line = sys.stdin.readline()
+        
+        if not line:
+            break
+            
+        a, b = map(int, line.split())
+        print(a + b)
+    except:
+        break
+    
