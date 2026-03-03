68cd59b89fc98e947e593195b4100f82e6e375f5
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-03-03 10:19:11 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,10 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+baskets = list(range(1, n + 1))
+
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    baskets[i-1: j] = baskets[i-1: j][::-1]
+
+print(*baskets)
