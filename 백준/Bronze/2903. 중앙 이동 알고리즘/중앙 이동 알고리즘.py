bab534fb3ad913d73af43838a419e1b1653f9275
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-03-13 09:15:37 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -1,10 +1,5 @@
 import sys
 
 n = int(sys.stdin.readline())
-side_vertices = 2
-
-for _ in range(n):
-    side_vertices = side_vertices * 2 - 1
-    
-print(side_vertices ** 2)
+print(((2 ** n) + 1) ** 2)
     
