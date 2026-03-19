1620d8781730c46f866aea77e69cf2abe3f86947
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-03-19 11:11:42 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+import sys
+
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(sys.stdin.readline())
+numbers = map(int, sys.stdin.readline().split())
+
+count = 0
+
+for num in numbers:
+    if is_prime(num):
+        count += 1
+        
+print(count)
