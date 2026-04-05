a244ed6acb088cec440e4a9ff3167aebb7ac0146
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-04-06 08:48:23 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,8 +1,8 @@
 import sys
 a, b, c, d, e, f = map(int, sys.stdin.readline().split())
+denominator = a*e - b*d
 
-for x in range(-999, 1000):
-    for y in range(-999, 1000):
-        if (a*x + b*y == c) and (d*x + e*y == f):
-            print(x, y)
-            break
+if denominator != 0:
+    x = (c*e - b*f) // denominator
+    y = (a*f - c*d) // denominator
+    print(x, y)
