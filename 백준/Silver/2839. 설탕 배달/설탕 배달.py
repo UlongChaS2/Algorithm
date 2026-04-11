d8ea265920480700ebfde7c1f18d99aa60fe4531
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-04-11 23:36:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+count = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+    
+    N -= 3
+    count += 1
+    
+    if N < 0:
+        print(-1)
+        break
