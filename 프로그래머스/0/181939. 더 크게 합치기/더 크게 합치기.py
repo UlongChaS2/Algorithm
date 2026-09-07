01b76c53ebce723968364f96e6a33ad112fae7e1
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-09-07 13:28:23 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,2 @@
 def solution(a, b):
-    sum_1 = str(a) + str(b)
-    sum_2 = str(b) + str(a)
-    
-    if int(sum_1) > int(sum_2):
-        return int(sum_1)
-    else:
-        return int(sum_2)
+	return int(max(f"{a}{b}", f"{b}{a}"))
