68c313399298cb6fba768cf6168bb09b637d0634
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-09-17 10:14:31 +0900

    [level 1] Title: 폰켓몬, Time: 0.54 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,4 @@
 def solution(nums):
-    dic = {}
-    for num in nums:
-        if num in dic:
-            dic[num] += 1
-        else:
-            dic[num] = 1
-            
-    return min(len(nums) // 2, len(dic.keys()))
+    # 무조건 해시 == object라는 생각으로 1 더했었는데 value는 사용을 안했었음
+    return min(len(nums) // 2, len(set(nums)))
     
