8b6c5a3256145340c250d393292e508cb3d2df3e
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-09-17 11:30:08 +0900

    [level 1] Title: 기사단원의 무기, Time: 884.99 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -2,7 +2,15 @@ def solution(number, limit, power):
     divisor_arr = []
     
     for num in range(1, number + 1):
-        count = sum(2 if i != num // i else 1 for i in range(1, int(num ** 0.5) + 1) if num % i == 0)
+        count = 0
+        i = 1
+        while i * i <= num:
+            if num % i == 0:
+                if i == num // i:
+                    count += 1
+                else:
+                    count += 2
+            i += 1
         divisor_arr.append(count)        
         
     for i in range(len(divisor_arr)):
