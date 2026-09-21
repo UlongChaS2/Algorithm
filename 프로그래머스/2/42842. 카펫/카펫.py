f60e44bdad77d83f6f5c000934621aeca7e81e8d
Jung Yujung <62024139+UlongChaS2@users.noreply.github.com>
2026-09-21 16:41:28 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,15 +1,11 @@
 def solution(brown, yellow):
-    total = brown + yellow 
-    fairs = []
+    total = brown + yellow
     i = 3
-    
     while i * i <= total:
         if total % i == 0:
-            fairs.append([total // i, i])
-        i += 1 
-    
-    for (w, h) in fairs:
-        if (w-2) * (h-2) == yellow:
-            return [w, h]
+            w, h = total // i, i
+            if (w - 2) * (h - 2) == yellow:
+                return [w, h]
+        i += 1
         
         
